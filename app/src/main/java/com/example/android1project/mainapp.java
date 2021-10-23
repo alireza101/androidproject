bc4b40eec64db3b00847f925ef0cf95654c7bf0e
Alireza101 <mohsenbarat007@mail.com>
2021-10-23 08:38:01 -0700

    v1 new commit

--- app/src/main/java/com/example/android1project/mainapp.java
+++ app/src/main/java/com/example/android1project/mainapp.java
@@ -4,19 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class mainapp extends AppCompatActivity implements IData{
-//    private ArrayList<String> mnamehor = new ArrayList<>();
-//    private ArrayList<Integer> mimagehor = new ArrayList<Integer>();
+
 
     static ArrayList<item> itemArrayList = new ArrayList<>();
+    ArrayList<item> itemArrayListfilter = new ArrayList<>();
+
     static ArrayList<type> typeArrayList = new ArrayList<>();
+    boolean flagfilter=false;
 
     webapihandler webapihandler;
     RecyclerView rcmain,rcmainhor;
@@ -29,10 +32,31 @@ public class mainapp extends AppCompatActivity implements IData{
         webapihandler.apiconecct("register");
         rcmain=findViewById(R.id.recyclerviewver);
         rcmainhor=findViewById(R.id.recyclerview);
+        if (!SharedPrefManager.getInstance(this).isLoggedIn()) {
+            finish();
+            startActivity(new Intent(this, login_activity.class));
+        }
         rcmain.addOnItemTouchListener(new RecyclerTouchListener(this, rcmain, new RecyclerTouchListener.ClickListener() {
             @Override
             public void onClick(View view, int position) {
-
+                Intent intent=new Intent(mainapp.this,detail_item.class);
+                item item;
+                if (flagfilter) {
+                    item = itemArrayListfilter.get(position);
+                }else {
+                    item = itemArrayList.get(position);
+                }
+                String getidname = "";
+                for (type type:typeArrayList){
+                    if (item.getItemtype().equals(type.getTypeid())){
+                        getidname=type.getTypename();
+                        break;
+                    }
+                }
+                String[]putitem=new String[]{item.getItemid(),item.getItemname(),item.getItempicture(),item.getItemsum()
+                        ,getidname,item.getItemexpiration()};
+                intent.putExtra("item",putitem);
+                startActivity(intent);
             }
 
             @Override
@@ -43,7 +67,21 @@ public class mainapp extends AppCompatActivity implements IData{
         rcmainhor.addOnItemTouchListener(new RecyclerTouchListener(this, rcmainhor, new RecyclerTouchListener.ClickListener() {
             @Override
             public void onClick(View view, int position) {
+                itemArrayListfilter.clear();
 
+                type type=typeArrayList.get(position);
+                for (item item : itemArrayList) {
+                    if (type.getTypename().equals("All")){
+                        itemArrayListfilter.add(item);
+                    }else {
+                        if (type.getTypeid().equals(item.getItemtype())) {
+                            itemArrayListfilter.add(item);
+                        }
+                    }
+                }
+                recyclerviewadapter_ver adapter = new recyclerviewadapter_ver(getApplicationContext(), itemArrayListfilter);
+                rcmain.setAdapter(adapter);
+                flagfilter=true;
 
             }
 
